dec49d0356ceeb7bf7f4c8f8ae8496ecef12a319
camperitox <ml17017@ues.edu.sv>
2024-06-09 12:37:27 -0600

    feat: Actualizar configuración de Angular para optimizar carga de archivos y estilos

---
+++ dist/contasync/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-R4SEZVKI.mjs";import"./chunk-MCRGOBGD.mjs";import"./chunk-SQB3MJEK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://sistemaplanillas.onrender.com/api',  // Cambia esto según tu configuración de producción
+};
+  
