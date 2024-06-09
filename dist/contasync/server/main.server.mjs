b2d694db78b6a70e71ac87e642f25f365dab5ed1
camperitox <ml17017@ues.edu.sv>
2024-06-08 23:03:43 -0600

    chore: Remove /dist from .gitignore

---
+++ dist/contasync/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-DD5PT6CH.mjs";import"./chunk-JLQYZSC7.mjs";import"./chunk-SQO2ILJX.mjs";import"./chunk-UFEVZCOA.mjs";import"./chunk-OT3SH22X.mjs";import"./chunk-MJKZWC6E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
